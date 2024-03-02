5e813854eb3e4ad836eb0237eeb97e38ebe08cc3
Martin <martinplr@gmail.com>
2024-03-02 12:49:06 +0100

    feat(meta) support id and previousValue in save()

--- app/Meta.php
+++ app/Meta.php
@@ -10,18 +10,27 @@ class Meta
     {
     }
 
-    public function get()
+    public function get($id = '')
     {
-        return DB::table('meta')
-            ->where(['type' => $this->type, 'key' => $this->key])
-            ->pluck('value')
-            ->transform(function ($value) {
-                if (str($value)->isJson()) {
-                    return json_decode($value, true);
-                }
+        $column = [
+            'type' => $this->type,
+            'key' => $this->key,
+        ];
 
-                return $value;
-            });
+        if ($id !== '') {
+            $column['id'] = $id;
+        }
+
+        $collection = DB::table('meta')
+            ->where($column)
+            ->get()
+            ->mapWithKeys(fn ($meta) => [$meta->id => str($meta->value)->isJson() ? json_decode($meta->value, true) : $meta->value]);
+
+        if ($id !== '') {
+            return $collection->first();
+        }
+
+        return $collection;
     }
 
     public function first()
@@ -31,35 +40,48 @@ class Meta
 
     public function insert($value)
     {
-        if (is_array($value)) {
-            $value = json_encode($value);
-        }
-
         return DB::table('meta')->insert([
             'type' => $this->type,
             'key' => $this->key,
-            'value' => $value,
+            'value' => is_array($value) ? json_encode($value) : $value,
         ]);
     }
 
-    public function save($value)
+    public function save($value, $previousValue = '', $id = '')
     {
-        if (is_array($value)) {
-            $value = json_encode($value);
+        $attributes = [
+            'type' => $this->type,
+            'key' => $this->key,
+        ];
+
+        if ($previousValue !== '') {
+            $attributes['value'] = is_array($previousValue) ? json_encode($previousValue) : $previousValue;
+        }
+
+        if ($id !== '') {
+            $attributes['id'] = $id;
         }
 
         return DB::table('meta')->updateOrInsert(
-            ['type' => $this->type, 'key' => $this->key],
-            ['value' => $value],
+            $attributes,
+            ['value' => is_array($value) ? json_encode($value) : $value]
         );
     }
 
-    public function delete($value = '')
+    public function delete($value = '', $id = '')
     {
-        $query = DB::table('meta')->where(['type' => $this->type, 'key' => $this->key]);
+        $query = DB::table('meta')
+            ->where([
+                'type' => $this->type,
+                'key' => $this->key,
+            ]);
 
         if ($value !== '') {
             $query->where('value', $value);
+        }
+
+        if ($id !== '') {
+            $query->where('id', $id);
         }
 
         return $query->delete();
