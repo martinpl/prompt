f33bff676536836c4193e3922aa018cfea72665c
Martin <martinplr@gmail.com>
2024-03-13 20:49:09 +0100

    style: move command action to seperate file (2)

---
+++ app/View/Components/Action/Command.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\Action;
+
+use App\View\Components\Action;
+
+class Command extends Action
+{
+    public function __construct(public $index, public $target, public $shortcut = false) 
+    {
+    }
+
+    public function commandUrl($currentExtension)
+    {
+        if (!$this->target) {
+            return;
+        }
+
+        if (!$currentExtension) {
+            $currentExtension = str(url()->full())->after('extensions/')->before('/');
+        }
+
+        return '/extensions/'.$currentExtension.'/'.$this->target;
+    }
+
+    public function render()
+    {
+        return view('components.action.command');
+    }
+}
