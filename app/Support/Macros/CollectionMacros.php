9ffdd982c97f0f376876c8e857ca77248274bf77
Martin <martinplr@gmail.com>
2024-03-23 16:37:18 +0100

    feat(CollectionMacros) meta collection methods

--- app/Support/Macros/CollectionMacros.php
+++ app/Support/Macros/CollectionMacros.php
@@ -9,10 +9,33 @@ class CollectionMacros
     public static function bootstrap()
     {
         self::index();
+        self::meta();
     }
 
     private static function index()
     {
         Collection::macro('index', fn ($index) => $this->slice($index, 1)->first());
+    }
+
+    private static function meta()
+    {
+        Collection::macro('pushWithMeta', function ($item, $meta) {
+            $meta->insert($item);
+
+            return $this->push($item);
+        });
+
+        Collection::macro('forgetWithMeta', function ($id, $meta) {
+            $meta->delete(id: $id);
+
+            return $this->forget($id);
+        });
+
+        Collection::macro('updateWithMeta', function ($item, $id, $meta) {
+            $meta->save($item, id: $id);
+            $this[$id] = $item;
+
+            return $this;
+        });
     }
 }
