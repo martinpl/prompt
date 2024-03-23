406cd7116061f7d440db6993e9949f54ee61ce7f
Martin <martinplr@gmail.com>
2024-03-23 13:34:05 +0100

    style(action-panel) rename "if" -> shouldRender

--- resources/views/components/action-panel.blade.php
+++ resources/views/components/action-panel.blade.php
@@ -1,6 +1,6 @@
-@props(['if' => !isset($this->selected) || $this->selected == self::$index])
+@props(['shouldRender' => !isset($this->selected) || $this->selected == self::$index])
 
-@pushIf($if, 'actions')
+@pushIf($shouldRender, 'actions')
     <div class="flex gap-2">
         <div class="flex gap-2 hover:bg-neutral-600 rounded-lg p-1" wire:click="enter">
             {!! $firstCommand($slot) !!}
