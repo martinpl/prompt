c4acb0e1f30856e310c76a43b1b48240dfea05fc
Martin <martinplr@gmail.com>
2024-03-24 11:55:20 +0100

    feat(dropdown) limit max items count on the screen

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,6 +1,6 @@
 @props(['index'])
 @php($tag = $attributes->has('href') ? 'a' : 'div')
 
-<{{ $tag }} {{ $attributes }} class="p-1 rounded-lg" :class="selected == {{ $index }} && 'bg-neutral-600 selected'" @click="show = false" data-id="item">
+<{{ $tag }} {{ $attributes }} class="block p-1 rounded-lg" :class="selected == {{ $index }} && 'bg-neutral-600 selected'" @click="show = false" data-id="item" data-index="{{ $index }}">
     {{ $slot }}
 </{{ $tag }}>
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,12 +1,14 @@
 @props(['postion' => 'left'])
 
 <div {{ $attributes }} class="relative" x-data="dropdown" @click.outside="show = false">
-    <div class="flex hover:bg-neutral-600" @click="open" {{ $head->attributes }}>
+    <div class="flex items-center hover:bg-neutral-600 rounded-lg p-1" @click="open" {{ $head->attributes }}>
         {{ $head }}
     </div>
     <div class="absolute bottom-6 {{ $postion == 'left' ? 'left-0' : 'right-0' }} pointer-events-none opacity-0 grid gap-2 w-52 bg-neutral-700 rounded-lg p-2" 
         :class="show && 'pointer-events-auto opacity-100'" data-id="body">
-        {{ $slot }}
+        <div class="max-h-36 overflow-y-auto grid gap-1">
+            {{ $slot }}
+        </div>
         <x-text-input x-ref="search" type="text" 
             @keyup.enter.stop="enter"
             @keyup.escape.stop="escape"
@@ -25,7 +27,7 @@
             this.$refs.search.focus()
         },
         count() {
-            return this.$root.querySelectorAll('[data-id="body"] > [data-id="item"]').length;
+            return this.$root.querySelectorAll('[data-id="body"] [data-id="item"]').length;
         },
         up() {
             this.selected = (this.selected - 1 + this.count()) % this.count()
@@ -40,6 +42,9 @@
             this.show = false;
             document.querySelector('input').focus()
         },
+        init() {
+            this.$watch('selected', value => this.$root.querySelector(`[data-index="${value}"]`).scrollIntoView(false))
+        }
     }))
 </script>
 @endscript
