f70e2f24201756ad0338ee69bf49005a3da76fb6
Martin <martinplr@gmail.com>
2024-04-05 22:33:13 +0200

    feat(command) allow to pass searchBarPlaceholder

--- resources/views/components/command.blade.php
+++ resources/views/components/command.blade.php
@@ -1,4 +1,7 @@
-@props(['afterSearch'])
+@props([
+    'searchBarPlaceholder' => 'Search...',
+    'afterSearch' => '',
+])
 
 <div class="h-screen flex flex-col bg-neutral-800 text-white rounded-xl overflow-hidden" wire:keyup.escape.window="escape">
     <header class="flex py-3 px-4 border-b border-neutral-700" wire:loading.class="border-loading">
@@ -8,9 +11,9 @@
             </button>
         @endif
         @isset($this->query)
-            <input type="text" id="search" placeholder="Search..." class="w-full text-xl focus:outline-0 bg-neutral-800" wire:model.live="query" autofocus>
+            <input type="text" id="search" placeholder="{{ $searchBarPlaceholder }}" class="w-full text-xl focus:outline-0 bg-neutral-800" wire:model.live="query" autofocus>
         @endisset
-        {{ $afterSearch ?? '' }}
+        {{ $afterSearch }}
     </header>
     <div {{ $attributes->merge(['class' => 'h-full overflow-x-auto m-2']) }}>
         {{ $slot }}
