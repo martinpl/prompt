296a1500d4c52e875cf5aefd85cbc170b7758059
Martin <martinplr@gmail.com>
2024-04-06 12:52:44 +0200

    feat(list-item) use icon component

--- resources/views/components/list-item.blade.php
+++ resources/views/components/list-item.blade.php
@@ -1,13 +1,16 @@
 
-@props(['icon', 'title', 'subtitle' => '', 'accessories'])
+@props([
+    'icon' => false, 
+    'title', 
+    'subtitle' => '', 
+    'accessories',
+])
 @aware(['filtering'])
 @php($attributes = $attributes->class('h-10 px-2 flex items-center justify-between'))
 
 <x-item :$title :$filtering :$attributes>
-    <div class="flex gap-2">
-        @isset ($icon)
-            <img src="{{ $icon['source'] }}" class="w-6 h-6 mr-2">
-        @endisset
+    <div class="flex items-center gap-2">
+        <x-icon :$icon />
         {{ $title }}
         @if ($subtitle)
             <div class="text-neutral-400">
